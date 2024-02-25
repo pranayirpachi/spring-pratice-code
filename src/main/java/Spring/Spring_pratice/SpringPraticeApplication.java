d30f1dcc8d257f0229f4ebf547e1696c54b2c7e2
App <pranayirpachi786@gmail.com>
2024-02-25 17:39:09 +0530

    initilise spring project

---
+++ src/main/java/Spring/Spring_pratice/SpringPraticeApplication.java
@@ -0,0 +1,16 @@
+package Spring.Spring_pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringPraticeApplication {
+
+	public static void main(String[] args)
+	{
+
+		SpringApplication.run(SpringPraticeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/Tea.java
@@ -0,0 +1,5 @@
+public class Tea {
+    public static void main(String[] args) {
+        System.out.println("welcome");
+    }
+}
